c67c50f1662a6b0aaf70fdcb6e7662d6c48926dd
Ashutosh Singh <44143541+iamsinghashutosh@users.noreply.github.com>
2020-04-13 06:21:07 +0530

    Create A_Filling_Diamonds.cpp

---
+++ Constructive Algorithms/A_Filling_Diamonds.cpp
@@ -0,0 +1,41 @@
+/*
+  GOOD is not Enough
+  You've got to be GREAT.
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+// #include<boost/multiprecision/cpp_int.hpp>
+// using namespace boost::multiprecision;
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define mod 1000000007
+#define FAST ios: :sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fp(i,k,n) for(int i=k;i<=n;i++)
+#define fr(i,k,n) for(int i=k;i>=n;i--)
+#define pb push_back
+#define F first
+#define S second
+#define all(a) a.begin(),a.end()
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+
+
+void solve()
+{ 
+  int n;
+  cin >> n;
+  cout << n << '\n';
+ return;
+}
+
+signed main()
+{
+  int T=1;
+  cin >> T;
+  while(T--)
+  {
+    solve();
+	}
+  return 0;
+}
