8da38e660f1c260f23fa52a2addcc09446e00130
Ashutosh Singh <44143541+iamsinghashutosh@users.noreply.github.com>
2020-04-22 17:17:29 +0530

    Add files via upload

---
+++ Constructive Algorithms/D_Constant_Palindrome_Sum.cpp
@@ -0,0 +1,64 @@
+/*
+  GOOD is not Enough
+  You've got to be GREAT.
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+// #include<boost/multiprecision/cpp_int.hpp>
+// using namespace boost::multiprecision;
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define mod 1000000007
+#define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fp(i,k,n) for(int i=k;i<=n;i++)
+#define fr(i,k,n) for(int i=k;i>=n;i--)
+#define pb push_back
+#define pii pair<int,int>
+#define dbg(x) cout << (#x) << " is " << (x) << '\n' 
+#define F first
+#define S second
+#define all(a) a.begin(),a.end()
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+
+void solve()
+{ 
+  int n,k;
+  cin >> n >> k;
+  map<int,int> m;
+  int a[n+1];
+  int pre[(2*k)+2]={0};
+  fp(i,1,n)
+   {
+       cin >> a[i];
+   }
+  fp(i,1,n/2)
+  {
+      m[a[i]+a[n-i+1]]++;
+      pre[min(a[i],a[n-i+1])+1]++;
+      pre[max(a[i],a[n-i+1])+k+1]--;
+  }
+  fp(i,1,2*k)
+    pre[i]+=pre[i-1];
+  int ans=mod;
+  for(auto x:m)
+  {
+      int sum=x.F;
+      ans=min({ans,n/2,pre[sum]-m[sum]+(n/2 - pre[sum])*2});
+  }
+  cout << ans << '\n';
+  return;
+}
+
+signed main()
+{
+  FAST
+  int T=1;
+   cin >> T;
+  while(T--)
+  {
+    solve();
+  }
+  return 0;
+}
