def8e82e33b667ac49bcb7b6a25c107e90bb8d09
Ashutosh Singh <44143541+iamsinghashutosh@users.noreply.github.com>
2020-04-23 19:29:44 +0530

    Add files via upload

---
+++ math/C_Sasha_and_a_Bit_of_Relax.cpp
@@ -0,0 +1,65 @@
+/*
+  GOOD is not Enough
+  You've got to be GREAT.
+*/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+// #include<boost/multiprecision/cpp_int.hpp>
+// using namespace boost::multiprecision;
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define mod 1000000007
+#define FAST ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fp(i,k,n) for(int i=k;i<=n;i++)
+#define fr(i,k,n) for(int i=k;i>=n;i--)
+#define pb push_back
+#define pii pair<int,int>
+#define dbg(x) cout << (#x) << " is " << (x) << '\n' 
+#define F first
+#define S second
+#define all(a) a.begin(),a.end()
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+
+void solve()
+{ 
+  int n;
+  cin >> n;
+  int x=0,ans=0;
+  int a[n+1];
+  map<int,int> ex,ox;
+  fp(i,1,n)
+  {
+      cin >> a[i];
+      x=a[i]^x;
+      if(i%2==0)
+        ans+=ex[x];
+      else
+        ans+=ox[x];
+      if(i%2==1)
+      {
+           ox[x]++;
+      }
+      else
+       {
+           if(x==0)
+            ans++;
+          ex[x]++;
+       }
+  }
+  cout << ans << '\n';
+  return;
+}
+
+signed main()
+{
+  FAST
+  int T=1;
+  //  cin >> T;
+  while(T--)
+  {
+    solve();
+  }
+  return 0;
+}
