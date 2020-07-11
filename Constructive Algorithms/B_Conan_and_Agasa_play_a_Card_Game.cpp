7340d869156975e873401826f3d18b55ef6e3ce8
Ashutosh Singh <sashutosh705@gmail.com>
2020-07-11 16:24:03 +0530

    Add files via upload

---
+++ Constructive Algorithms/B_Conan_and_Agasa_play_a_Card_Game.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define int         long long
+#define mod         1000000007
+#define FAST        ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define f(i,n)      for(int i=0;i<n;i++)
+#define fp(i,k,n)   for(int i=k;i<=n;i++)
+#define fr(i,k,n)   for(int i=k;i>=n;i--)
+#define pb          push_back
+#define pii         pair<int,int>
+#define vi          vector<int>
+#define vii         vector<pii>
+#define dbg(x)      cout << (#x) << " is " << (x) << '\n' 
+#define F           first
+#define S           second
+#define sz(x)       (int)(x).size()
+#define lb          lower_bound
+#define ub          upper_bound
+#define mems(x)     memset(x,0,sizeof(x))
+#define all(a)      a.begin(),a.end()
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+/*---------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+  int n;
+  cin >> n;
+  map<int,int> m;
+  f(i,n)
+  {
+      int x;
+      cin >> x;
+      m[x]++;
+  }
+  for(auto x:m)
+  {
+      if((x.S)&1)
+      {
+          cout << "Conan\n";return;
+      }
+  }
+  cout << "Agasa\n";
+  return;
+} 
+
+signed main()
+{
+  FAST
+  int T=1;
+//   cin >> T;
+  while(T--)
+  {
+    solve();
+  }
+  return 0;
+}
